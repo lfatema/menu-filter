22cb53599051470249cb9045721c6ccdcc399011
fatema_lokhand <lokhandwala789@gmail.com>
2023-03-28 14:52:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react' //rafce (react arrow function component export)
+import Restaurant from './Component/Basics/Restaurant'
+
+const App = () => {
+  return (
+    <>
+   <Restaurant/>
+    </>
+ 
+  ) 
+}
+
+export default App
